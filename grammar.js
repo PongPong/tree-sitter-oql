96bbf544c8f3b13e9c79fe7a582501f9886c4661
Pong Lam <chunpong.lam@williamhill.co.uk>
2021-12-13 17:11:39 +0000

    fix: remove keyword_* from syntax tree

--- grammar.js
+++ grammar.js
@@ -35,37 +35,37 @@ module.exports = grammar({
   rules: {
     source_file: $ => repeat($._statement),
 
-    keyword_describe: _$ => kw("DESCRIBE"),
-    keyword_select: _$ => kw("SELECT"),
-    keyword_as: _$ => kw("AS"),
-    keyword_from: _$ => kw("FROM"),
-    keyword_left: _$ => kw("LEFT"),
-    keyword_right: _$ => kw("RIGHT"),
-    keyword_full: _$ => kw("FULL"),
-    keyword_inner: _$ => kw("INNER"),
-    keyword_outer: _$ => kw("OUTER"),
-    keyword_where: _$ => kw("WHERE"),
-    keyword_having: _$ => kw("HAVING"),
-    keyword_groupby: _$ => kw("GROUP BY"),
-    keyword_orderby: _$ => kw("ORDER BY"),
-    keyword_asc: _$ => kw("ASC"),
-    keyword_desc: _$ => kw("DESC"),
-    keyword_offset: _$ => kw("OFFSET"),
-    keyword_limit: _$ => kw("LIMIT"),
-    keyword_join: _$ => kw("JOIN"),
-    keyword_on: _$ => kw("ON"),
-    keyword_null: _$ => kw("NULL"),
-    keyword_true: _$ => kw("TRUE"),
-    keyword_false: _$ => kw("FALSE"),
-    keyword_not: _$ => kw("NOT"),
-    keyword_and: _$ => kw("AND"),
-    keyword_or: _$ => kw("OR"),
-    keyword_between: _$ => kw("BETWEEN"),
-    keyword_is: _$ => kw("IS"),
-    keyword_in: _$ => kw("IN"),
-    keyword_like: _$ => kw("LIKE"),
-    keyword_regexp: _$ => kw("REGEXP"),
-    keyword_exists: _$ => kw("EXISTS"),
+    _keyword_describe: _$ => kw("DESCRIBE"),
+    _keyword_select: _$ => kw("SELECT"),
+    _keyword_as: _$ => kw("AS"),
+    _keyword_from: _$ => kw("FROM"),
+    _keyword_left: _$ => kw("LEFT"),
+    _keyword_right: _$ => kw("RIGHT"),
+    _keyword_full: _$ => kw("FULL"),
+    _keyword_inner: _$ => kw("INNER"),
+    _keyword_outer: _$ => kw("OUTER"),
+    _keyword_where: _$ => kw("WHERE"),
+    _keyword_having: _$ => kw("HAVING"),
+    _keyword_groupby: _$ => kw("GROUP BY"),
+    _keyword_orderby: _$ => kw("ORDER BY"),
+    _keyword_asc: _$ => kw("ASC"),
+    _keyword_desc: _$ => kw("DESC"),
+    _keyword_offset: _$ => kw("OFFSET"),
+    _keyword_limit: _$ => kw("LIMIT"),
+    _keyword_join: _$ => kw("JOIN"),
+    _keyword_on: _$ => kw("ON"),
+    _keyword_null: _$ => kw("NULL"),
+    _keyword_true: _$ => kw("TRUE"),
+    _keyword_false: _$ => kw("FALSE"),
+    _keyword_not: _$ => kw("NOT"),
+    _keyword_and: _$ => kw("AND"),
+    _keyword_or: _$ => kw("OR"),
+    _keyword_between: _$ => kw("BETWEEN"),
+    _keyword_is: _$ => kw("IS"),
+    _keyword_in: _$ => kw("IN"),
+    _keyword_like: _$ => kw("LIKE"),
+    _keyword_regexp: _$ => kw("REGEXP"),
+    _keyword_exists: _$ => kw("EXISTS"),
 
     _statement: $ =>
       seq(
@@ -79,7 +79,7 @@ module.exports = grammar({
     /**
       * Describe table
       */
-    describe_statement: $ => seq($.keyword_describe, $.identifier),
+    describe_statement: $ => seq($._keyword_describe, $.identifier),
 
     /** 
       * Statements and Clauses
@@ -96,11 +96,11 @@ module.exports = grammar({
         optional($.limit_clause)
       )),
     select_clause: $ =>
-      prec.left(seq($.keyword_select, choice(
+      prec.left(seq($._keyword_select, choice(
         '*',
         commaSep1($._aliasable_expression),
       ))),
-    _aliased_expression: $ => seq($._expression, optional($.keyword_as), $.identifier),
+    _aliased_expression: $ => seq($._expression, optional($._keyword_as), $.identifier),
     _aliasable_expression: $ =>
       choice($._expression, alias($._aliased_expression, $.alias)),
     _query_table_expression_clause: $ => choice(
@@ -108,45 +108,45 @@ module.exports = grammar({
       $._parenthesized_subquery
     ),
     from_clause: $ => prec.left(seq(
-      $.keyword_from,
+      $._keyword_from,
       commaSep1($._query_table_expression_clause)
     )),
     join_type: $ =>
       seq(
         choice(
-          $.keyword_inner,
+          $._keyword_inner,
           seq(
-            choice($.keyword_left, $.keyword_right, $.keyword_full),
-            optional($.keyword_outer),
+            choice($._keyword_left, $._keyword_right, $._keyword_full),
+            optional($._keyword_outer),
           ),
         ),
       ),
     join_clause: $ =>
       seq(
         optional($.join_type),
-        $.keyword_join,
+        $._keyword_join,
         $.identifier,
-        $.keyword_on,
+        $._keyword_on,
         $._expression,
       ),
-    where_clause: $ => seq($.keyword_where, $._condition),
-    having_condition: $ => seq($.keyword_having, $._condition),
+    where_clause: $ => seq($._keyword_where, $._condition),
+    having_condition: $ => seq($._keyword_having, $._condition),
     group_by_clause: $ => prec.left(
-      seq($.keyword_groupby, commaSep1($._expression), optional($.having_condition))),
+      seq($._keyword_groupby, commaSep1($._expression), optional($.having_condition))),
     order_by_clause: $ => prec.left(
-      seq($.keyword_orderby, commaSep1($._expression),
-        optional(choice($.keyword_asc, $.keyword_desc)))),
-    _offset_clause: $ => seq($.keyword_offset, field("offset", $._number)),
+      seq($._keyword_orderby, commaSep1($._expression),
+        optional(choice($._keyword_asc, $._keyword_desc)))),
+    _offset_clause: $ => seq($._keyword_offset, field("offset", $._number)),
     limit_clause: $ => prec.left(choice(
-      seq($.keyword_limit, field("limit", $._number), optional($._offset_clause)),
+      seq($._keyword_limit, field("limit", $._number), optional($._offset_clause)),
       $._offset_clause,
-      seq($.keyword_limit, field("offset", $._number), ",", field("limit", $._number)),
+      seq($._keyword_limit, field("offset", $._number), ",", field("limit", $._number)),
     )),
     parameter: $ => seq($.identifier, $._type),
     parameters: $ => seq("(", commaSep1($.parameter), ")"),
-    NULL: $ => $.keyword_null,
-    TRUE: $ => $.keyword_true,
-    FALSE: $ => $.keyword_false,
+    NULL: $ => $._keyword_null,
+    TRUE: $ => $._keyword_true,
+    FALSE: $ => $._keyword_false,
     integer: _$ => token(/[+-]?[0-9]+/),
     _number: $ => choice(
       $.integer,
@@ -161,7 +161,7 @@ module.exports = grammar({
       seq('&', $.identifier),
     field_access: $ => seq($.identifier, "->>", $.string),
     ordered_expression: $ =>
-      seq($._expression, field("order", choice($.keyword_asc, $.keyword_desc))),
+      seq($._expression, field("order", choice($._keyword_asc, $._keyword_desc))),
     _type_alias: $ => alias($.identifier, $.type),
     array_type: $ => seq($._type, "[", "]"),
     _type: $ => choice($._type_alias, $.array_type),
@@ -199,39 +199,39 @@ module.exports = grammar({
     compound_condition: $ =>
       choice(
         prec.left(seq('(', $._condition, ')')),
-        prec.left('logical_not', seq($.keyword_not, $._condition)),
-        prec.left('logical_and', seq($._condition, $.keyword_and, $._condition)),
-        prec.left('logical_or', seq($._condition, $.keyword_or, $._condition)),
+        prec.left('logical_not', seq($._keyword_not, $._condition)),
+        prec.left('logical_and', seq($._condition, $._keyword_and, $._condition)),
+        prec.left('logical_or', seq($._condition, $._keyword_or, $._condition)),
       ),
     range_condition: $ =>
       prec.left(seq(
         $._expression,
-        optional($.keyword_not),
-        $.keyword_between,
+        optional($._keyword_not),
+        $._keyword_between,
         $._expression,
-        $.keyword_and,
+        $._keyword_and,
         $._expression)),
     null_condition: $ =>
       prec.left(choice(
         $._expression,
-        seq($._expression, $.keyword_is, optional($.keyword_not), $.keyword_null)
+        seq($._expression, $._keyword_is, optional($._keyword_not), $._keyword_null)
       )),
     membership_condition: $ =>
       prec.left('membership',
         choice(
-          seq($._expression, optional($.keyword_not), $.keyword_in,
+          seq($._expression, optional($._keyword_not), $._keyword_in,
             choice(
               $._expression_list,
               $._parenthesized_subquery
             )),
-          seq($._expression_list, optional($.keyword_not), $.keyword_in, '(',
+          seq($._expression_list, optional($._keyword_not), $._keyword_in, '(',
             commaSep1(choice($._expression_list, $._subquery)), ')')
         )
       ),
     like_condition: $ =>
-      prec.left(seq($._expression, optional($.keyword_not), $.keyword_like, $._expression)),
+      prec.left(seq($._expression, optional($._keyword_not), $._keyword_like, $._expression)),
     regexp_condition: $ =>
-      prec.left(seq($._expression, optional($.keyword_not), $.keyword_regexp, $._expression)),
+      prec.left(seq($._expression, optional($._keyword_not), $._keyword_regexp, $._expression)),
     pattern_matching_condition: $ => choice(
       $.like_condition,
       $.regexp_condition,
@@ -239,8 +239,8 @@ module.exports = grammar({
     _parenthesized_subquery: $ => seq('(', $._subquery, ')'),
     exists_condition: $ =>
       prec.left('exists', seq(
-        // optional($.keyword_not), // convered by compound condition
-        $.keyword_exists,
+        // optional($._keyword_not), // convered by compound condition
+        $._keyword_exists,
         $._parenthesized_subquery,
       )),
     _condition: $ =>
